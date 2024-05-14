45cd0e536e290c5001de6b2bcebde01529df06e3
Ikumaru Hamanaka <m21122@g.metro-cit.ac.jp>
2024-05-14 13:48:23 +0900

    word2vec

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-import streamlit as st
+from fastapi import FastAPI, HTTPException
+import gensim
+import gdown
+import zipfile
+import shutil
 
-st.write("Hello world")
+app = FastAPI()
+
+# Google Drive file ID
+file_id = "0ByFQ96A4DgSPUm9wVWRLdm5qbmc"
+url = f"https://drive.google.com/uc?id={file_id}&export=download"
+output = "download.zip"
+gdown.download(url, output, quiet=False)
+shutil.unpack_archive('download.zip')
+
+print("Download complete.")
+model = gensim.models.KeyedVectors.load_word2vec_format('model.vec', binary=False)
+
+@app.get("/word_merge/")
+async def merge_word(str1: str="", str2: str="", op: str="+"):
+    print(op)
+    if op == "plus":
+        return {"status": 200, "str": model.most_similar(positive=[str1,str2])}
+    if op == "minus":
+        return {"status": 200, "str": model.most_similar(positive=[str1],negative=[str2])}
+    return {"status": 400, "detail": "op not found"}
